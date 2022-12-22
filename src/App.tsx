169f34bbbcba4a4893998d0fb5848301d0e5db67
JoshBiong <joshuabiong7@gmail.com>
2022-12-23 03:04:06 +0800

    clering some lines

--- src/App.tsx
+++ src/App.tsx
@@ -22,24 +22,6 @@ function App() {
 //     return Math.floor(Math.random() * (max - min + 1)) + min;
 //   }
 
-//   const handleClick = () => {
-//     setNum(randomNumberInRange(1, 5));
-//   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   const [point, setPoint] = useState<TPoint[]>([]);
   const [popped, setPopped] = useState<TPoint[]>([]);
